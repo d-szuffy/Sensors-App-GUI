6b0d590905546c0cac1b9a5d5c82f82d9a5050e4
Mateusz <72738337+d-szuffy@users.noreply.github.com>
2020-11-27 08:41:44 +0100

    Main
    Main script with events handling, etc.

---
+++ main.py
@@ -0,0 +1,137 @@
+import sys
+from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.QtCore import Qt, QPropertyAnimation
+from matplotlib.backends.backend_qt5agg import (NavigationToolbar2QT as NavigationToolbar)
+
+from demo_main_window import *
+import numpy as np
+import random
+
+
+WINDOW_SIZE = 0
+
+
+class MyForm(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.setWindowFlag(Qt.FramelessWindowHint)
+        # self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
+
+ # Button click events to our top bar buttons
+        #
+        #Minimize window
+        self.ui.minimizeButton.clicked.connect(lambda: self.showMinimized())
+        #Close window
+        self.ui.closeButton.clicked.connect(lambda: self.close())
+        #Restore/Maximize window
+        self.ui.restoreButton.clicked.connect(lambda: self.restore_or_maximize_window())
+
+        self.ui.stackedWidget.setCurrentWidget(self.ui.settings_page)
+
+        self.ui.home_button.clicked.connect(lambda: self.ui.stackedWidget.setCurrentWidget(self.ui.home_page))
+        self.ui.accounts_button.clicked.connect(lambda: self.ui.stackedWidget.setCurrentWidget(self.ui.accounts_page))
+        self.ui.settings_button.clicked.connect(lambda: self.ui.stackedWidget.setCurrentWidget(self.ui.settings_page))
+
+        self.ui.pushButton_generate_random_signal.clicked.connect(lambda: self.updateGraph())
+
+        self.addToolBar(NavigationToolbar(self.ui.MplWidget.canvas, self.ui.MplWidget))
+
+
+    # Restore or maximize your window
+
+
+        def moveWindow(e):
+            # Detect if the window is  normal size
+            # ###############################################
+            if self.isMaximized() == False:  # Not maximized
+                # Move window only when window is normal size
+                # ###############################################
+                # if left mouse button is clicked (Only accept left mouse button clicks)
+                if e.buttons() == Qt.LeftButton:
+                    # Move window
+                    self.move(self.pos() + e.globalPos() - self.clickPosition)
+                    self.clickPosition = e.globalPos()
+                    e.accept()
+            # ###############################################
+
+
+        self.ui.main_header.mouseMoveEvent = moveWindow
+
+        self.ui.pushButton.clicked.connect(lambda: self.slideLeftMenu())
+
+
+        self.show()
+
+
+    def updateGraph(self):
+
+        fs = 500
+        f = random.randint(1, 100)
+        ts = 1 / fs
+        length_of_signal = 100
+        t = np.linspace(0, 1, length_of_signal)
+
+        cosinus_signal = np.cos(2 * np.pi * f * t)
+        sinus_signal = np.sin(2 * np.pi * f * t)
+
+        self.ui.MplWidget.canvas.axes.clear()
+        self.ui.MplWidget.canvas.axes.plot(t, cosinus_signal)
+        self.ui.MplWidget.canvas.axes.plot(t, sinus_signal)
+        self.ui.MplWidget.canvas.axes.legend(('cosinus', 'sinus'), loc='upper left')
+        self.ui.MplWidget.canvas.axes.set_title('Cosinus - Sinus Signal')
+        self.ui.MplWidget.canvas.draw()
+
+
+    def mousePressEvent(self, event):
+        # ###############################################
+        # Get the current position of the mouse
+        self.clickPosition = event.globalPos()
+        # We will use this value to move the window
+        # ###############################################
+
+    # ###############################################
+
+    def restore_or_maximize_window(self):
+
+# Global windows state
+        global WINDOW_SIZE #The default value is zero to show that the size is not maximized
+        win_status = WINDOW_SIZE
+
+        if win_status == 0:
+# If the window is not maximized
+        	WINDOW_SIZE = 1
+# Update value to show that the window has been maximized
+        	self.showMaximized()
+# Update button icon
+# self.ui.restoreButton.setIcon(QtGui.QIcon(u":/icons/icons/cil-window-maximize.png"))#Show maximized icon
+        else:
+# If the window is on its default size
+            WINDOW_SIZE = 0 #Update value to show that the window has been minimized/set to normal size (which is 800 by 400)
+            self.showNormal()
+# Update button icon
+# self.ui.restoreButton.setIcon(QtGui.QIcon(u":/icons/icons/cil-window-restore.png"))#Show minized icon
+
+    def slideLeftMenu(self):
+
+        width = self.ui.left_side_menu.width()
+
+        if width == 50:
+            newwidth = 150
+        else:
+            newwidth = 50
+
+        self.animation = QPropertyAnimation(self.ui.left_side_menu, b"minimumWidth")
+        self.animation.setDuration(250)
+        self.animation.setStartValue(width)
+        self.animation.setEndValue(newwidth)
+        self.animation.setEasingCurve(QtCore.QEasingCurve.InOutQuart)
+        self.animation.start()
+
+if __name__ == "__main__":
+    App = QApplication(sys.argv)
+    window = MyForm()
+    window.show()
+    sys.exit(App.exec())
